493c9a9821befc7e4d898cfa54fff79c676707b3
TheChosenHero <thechosenherotch@gmail.com>
2018-08-10 17:53:40 -0500

    basic window
    created basic window with a JFrame

--- src/dev/glick/asteroids/Launcher.java
+++ src/dev/glick/asteroids/Launcher.java
@@ -1,9 +1,11 @@
 package dev.glick.asteroids;
 
+import dev.glick.asteroids.display.Display;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-
+		new Display("Title!",500,500);
 	}
 
 }
